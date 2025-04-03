0b839b48b66b0048d77f7d606a7328e821c75c95
iFall8 <113339883+iFall8@users.noreply.github.com>
2025-04-03 14:21:21 +0700

    Add initial views, controllers, and styles for the application layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PeriksaController;
+use App\Http\Controllers\ObatController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dokter', [HomeController::class, 'dokter'])->name('dokter');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('dokter')->group(function() {
+    Route::resource('obat', ObatController::class);
+    Route::resource('periksa', PeriksaController::class);
 });
